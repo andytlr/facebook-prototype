5c437ca7a1a60718f259dc8532b5e73de9b95d2d
Andy Taylor <andytlr@gmail.com>
2015-10-22 14:09:09 -0700

    Normal Tasks Done

---
+++ facebook-prototype/CreateStatusViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CreateStatusViewController.swift
+//  facebook-prototype
+//
+//  Created by Andy Taylor on 10/22/15.
+//  Copyright © 2015 Andy Taylor. All rights reserved.
+//
+
+import UIKit
+
+class CreateStatusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func tapCancel(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
