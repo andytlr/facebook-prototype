7d12de05f1d308134f90cf5bf02a0f28dad3450c
Andy Taylor <andytlr@gmail.com>
2015-10-22 14:03:26 -0700

    4 done

---
+++ facebook-prototype/NotificationsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NotificationsViewController.swift
+//  facebook-prototype
+//
+//  Created by Andy Taylor on 10/22/15.
+//  Copyright © 2015 Andy Taylor. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.contentSize = imageView.frame.size
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ facebook-prototype/ThrillistDetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ThrillistDetailViewController.swift
+//  facebook-prototype
+//
+//  Created by Andy Taylor on 10/22/15.
+//  Copyright © 2015 Andy Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ThrillistDetailViewController: UIViewController {
+
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        scrollView.contentSize = imageView.frame.size
+
+    }
+    @IBAction func tapLike(sender: AnyObject) {
+        likeButton.selected = !likeButton.selected
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func tapBackButton(sender: AnyObject) {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
